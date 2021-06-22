921a33006e8b1f01aafa1b9b13ac5791fb19023f
LukasMurdock <lukas.murdock@gmail.com>
2021-06-22 13:06:49 -0400

    init

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Next Starter</title>
+      </Head>
+      <div className="prose">
+        <h1>Title</h1>
+      </div>
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+
+module.exports = {
+  purge: ['./pages/**/*.{js,mdx}', './components/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography')
+  ]
+}
